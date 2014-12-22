27c869b0558fa2473a6b809ffe63132affa9a4ec
chinhnc <chinhharubn@gmail.com>
2014-12-22 21:03:20 +0700

    add file thanhtoan

---
+++ view/thanhtoan.php
@@ -0,0 +1,48 @@
+<!-- BEGIN: main -->
+<link href="cart.css" rel="stylesheet" type="text/css" />
+   
+        <div class="content-right">
+          <h2 class="title-sale">Giỏ hàng</h2>
+          <div class="table-box">
+          <table class="table-stacked table shoppingCartTable table-bordered" id="shoppingCartTable">
+            <tbody>
+              <tr>
+                <th>Remove</th>
+                <th>Sản phẩm</th>
+                <th>Giá</th>
+                <th>Số lượng</th>
+                <th>Tổng</th>
+              </tr>
+              <tr>
+                <td class="action"><div class="remove-link"> <a class="icon-trash jsConfirm" href="" title="Xóa&quot;"> </a> </div></td>
+                <td class="product"><div class="product-name"> <a href="" title="Quần jeans ống đứng">Dây chuyền kim cương</a> </div></td>
+                <td class="price"> 450.000.000&nbsp;₫ </td>
+                <td class="quantity"><input name="quantity[5451cec5ab49be65218b45f4]" value="1" type="number"></td>
+                <td class="total"> 450.000.000&nbsp;₫ </td>
+              </tr>
+              <tr>
+                <td class="action"><div class="remove-link"> <a class="icon-trash jsConfirm" href="" title="Xóa&quot;"> </a> </div></td>
+                <td class="product"><div class="product-name"> <a href="">Đồng hồ kim cương</a> </div></td>
+                <td class="price"> 230.000.000&nbsp;₫ </td>
+                <td class="quantity"><input name="quantity[5451cec5ab49be65218b45f6]" value="1" type="number"></td>
+                <td class="total"> 230.000.000&nbsp;₫ </td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td class="action"></td>
+                <td class="product"></td>
+                <td class="price"><b>Tổng cộng: </b></td>
+                <td class="quantity"></td>
+                <td class="total">680.000.000&nbsp;₫</td>
+              </tr>
+            </tfoot>
+          </table>
+          </div>
+          <div class="cart-button">
+          		<a class="btn" href="product.html">Tiếp tục mua hàng</a>
+                <input class="btn btn-update" value="Cập nhật giỏ hàng" type="submit" />
+                <input class="btn btn-checkout"value="Thanh toán" type="submit" />
+          </div>
+      </div>
+<!-- END: main -->
